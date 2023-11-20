dd7fbcd7f1ef7bf0805fda2cc0ab72bd2aae981b
Spencer C. Imbleau <spencer@imbleau.com>
2023-11-20 03:41:11 -0500

    feat: expose `Vector`

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -4,6 +4,7 @@ mod parser;
 pub(crate) mod vector;
 
 pub use asset_loader::VelloVectorLoader;
+pub use vector::Vector;
 pub use vector::VelloVector;
 
 pub(crate) use asset_loader::VectorLoaderError;
