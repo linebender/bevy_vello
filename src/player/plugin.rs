b496d905bb9916e3a99c99ac16e2f7903a43f495
Spencer C. Imbleau <spencer@imbleau.com>
2024-02-15 10:44:31 -0500

    fix: race condition on rendering

--- src/player/plugin.rs
+++ src/player/plugin.rs
@@ -5,13 +5,11 @@ pub struct LottiePlayerPlugin;
 
 impl Plugin for LottiePlayerPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(
-            PreUpdate,
-            (systems::spawn_playheads, systems::advance_playheads).chain(),
-        )
-        .add_systems(
-            PostUpdate,
-            (systems::run_transitions, systems::transition_state).chain(),
-        );
+        app.add_systems(PreUpdate, systems::advance_playheads)
+            .add_systems(
+                PostUpdate,
+                (systems::run_transitions, systems::transition_state).chain(),
+            )
+            .add_systems(Last, systems::spawn_playheads);
     }
 }
