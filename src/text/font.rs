cca085c313bd10c298aff8db4dcf3bd198219f36
Spencer C. Imbleau <spencer@imbleau.com>
2024-03-14 16:26:48 -0400

    fix: text not rendering correctly

--- src/text/font.rs
+++ src/text/font.rs
@@ -1,13 +1,16 @@
 use super::vello_text::VelloText;
-use bevy::prelude::*;
 use bevy::reflect::TypePath;
 use bevy::render::render_asset::RenderAsset;
+use bevy::{prelude::*, time};
 use std::sync::Arc;
+use std::time::Instant;
 use vello::glyph::skrifa::{FontRef, MetadataProvider};
 use vello::glyph::Glyph;
 use vello::kurbo::Affine;
-use vello::peniko::{self, Blob, Font};
+use vello::peniko::{self, Blob, Brush, Color, Font};
 use vello::Scene;
+
+const VARIATIONS: &[(&str, f32)] = &[];
 
 #[derive(Asset, TypePath, Clone)]
 pub struct VelloFont {
@@ -43,7 +46,6 @@ impl VelloFont {
         let font_size = vello::skrifa::instance::Size::new(text.size);
         let charmap = font.charmap();
         let axes = font.axes();
-        const VARIATIONS: &[(&str, f32)] = &[];
         let var_loc = axes.location(VARIATIONS);
         let metrics = font.metrics(font_size, &var_loc);
         let line_height = metrics.ascent - metrics.descent + metrics.leading;
@@ -61,6 +63,7 @@ impl VelloFont {
             }
             let gid = charmap.map(ch).unwrap_or_default();
             let advance = glyph_metrics.advance_width(gid).unwrap_or_default();
+
             pen_x += advance;
             width = width.max(pen_x);
         }
@@ -74,7 +77,6 @@ impl VelloFont {
         let font_size = vello::skrifa::instance::Size::new(text.size);
         let charmap = font.charmap();
         let axes = font.axes();
-        const VARIATIONS: &[(&str, f32)] = &[];
         let var_loc = axes.location(VARIATIONS);
         let metrics = font.metrics(font_size, &var_loc);
         let line_height = metrics.ascent - metrics.descent + metrics.leading;
@@ -82,30 +84,36 @@ impl VelloFont {
 
         let mut pen_x = 0f32;
         let mut pen_y = 0f32;
+        let mut glyphs: Vec<Glyph> = text
+            .content
+            .chars()
+            .filter_map(|ch| {
+                if ch == '\n' {
+                    pen_y += line_height;
+                    pen_x = 0.0;
+                    return None;
+                }
+                let gid = charmap.map(ch).unwrap_or_default();
+                let advance = glyph_metrics.advance_width(gid).unwrap_or_default();
+                let x = pen_x;
+                pen_x += advance;
+                Some(Glyph {
+                    id: gid.to_u16() as u32,
+                    x,
+                    y: pen_y,
+                })
+            })
+            .collect();
+        // Push text up
+        glyphs.iter_mut().for_each(|g| {
+            g.y -= pen_y;
+        });
         scene
             .draw_glyphs(&self.font)
             .font_size(text.size)
             .transform(transform)
             .normalized_coords(var_loc.coords())
-            .brush(&text.brush.clone().unwrap_or_default())
-            .draw(
-                &vello::kurbo::Stroke::new(1.0),
-                text.content.chars().filter_map(|ch| {
-                    if ch == '\n' {
-                        pen_y += line_height;
-                        pen_x = 0.0;
-                        return None;
-                    }
-                    let gid = charmap.map(ch).unwrap_or_default();
-                    let advance = glyph_metrics.advance_width(gid).unwrap_or_default();
-                    let x = pen_x;
-                    pen_x += advance;
-                    Some(Glyph {
-                        id: gid.to_u16() as u32,
-                        x,
-                        y: pen_y,
-                    })
-                }),
-            )
+            .brush(&text.brush.clone().unwrap_or(Brush::Solid(Color::WHITE)))
+            .draw(vello::peniko::Fill::EvenOdd, glyphs.into_iter());
     }
 }
