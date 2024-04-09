56420a579a3e1a74546c5f3c59dde8679b528343
Spencer C. Imbleau <spencer@imbleau.com>
2024-04-09 14:19:30 -0400

    refactor: remove nesting

--- examples/scene/src/main.rs
+++ examples/scene/src/main.rs
@@ -20,28 +20,28 @@ fn setup_vector_graphics(mut commands: Commands) {
 
 fn simple_animation(mut query_scene: Query<(&mut Transform, &mut VelloScene)>, time: Res<Time>) {
     let sin_time = time.elapsed_seconds().sin().mul_add(0.5, 0.5);
+    let (mut transform, mut scene) = query_scene.single_mut();
 
-    for (mut transform, mut scene) in query_scene.iter_mut() {
-        *scene = VelloScene::default();
+    // Reset scene every frame
+    *scene = VelloScene::default();
 
-        // Animate color green to blue
-        let c = Vec3::lerp(
-            Vec3::new(-1.0, 1.0, -1.0),
-            Vec3::new(-1.0, 1.0, 1.0),
-            sin_time + 0.5,
-        );
+    // Animate color green to blue
+    let c = Vec3::lerp(
+        Vec3::new(-1.0, 1.0, -1.0),
+        Vec3::new(-1.0, 1.0, 1.0),
+        sin_time + 0.5,
+    );
 
-        // Animate the corner radius
-        scene.fill(
-            peniko::Fill::NonZero,
-            kurbo::Affine::default(),
-            peniko::Color::rgb(c.x as f64, c.y as f64, c.z as f64),
-            None,
-            &kurbo::RoundedRect::new(-50.0, -50.0, 50.0, 50.0, (sin_time as f64) * 50.0),
-        );
+    // Animate the corner radius
+    scene.fill(
+        peniko::Fill::NonZero,
+        kurbo::Affine::default(),
+        peniko::Color::rgb(c.x as f64, c.y as f64, c.z as f64),
+        None,
+        &kurbo::RoundedRect::new(-50.0, -50.0, 50.0, 50.0, (sin_time as f64) * 50.0),
+    );
 
-        transform.scale = Vec3::lerp(Vec3::ONE * 0.5, Vec3::ONE * 1.0, sin_time);
-        transform.translation = Vec3::lerp(Vec3::X * -100.0, Vec3::X * 100.0, sin_time);
-        transform.rotation = Quat::from_rotation_z(-std::f32::consts::TAU * sin_time);
-    }
+    transform.scale = Vec3::lerp(Vec3::ONE * 0.5, Vec3::ONE * 1.0, sin_time);
+    transform.translation = Vec3::lerp(Vec3::X * -100.0, Vec3::X * 100.0, sin_time);
+    transform.rotation = Quat::from_rotation_z(-std::f32::consts::TAU * sin_time);
 }
