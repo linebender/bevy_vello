af5a31a214c22e15dc9f29d7c6956d8fbfa62ebf
Sebastian Hamel <sebjfk@gmail.com>
2024-04-08 22:50:13 -0400

    remove unused finish function

--- src/render/plugin.rs
+++ src/render/plugin.rs
@@ -1,5 +1,5 @@
 use super::extract::{self, ExtractedPixelScale, SSRenderTarget};
-use super::{prepare, systems, BevyVelloRenderer, LottieRenderer};
+use super::{prepare, systems, LottieRenderer};
 use crate::render::extract::ExtractedRenderText;
 use crate::render::SSRT_SHADER_HANDLE;
 use crate::{VelloCanvasMaterial, VelloFont};
@@ -10,7 +10,6 @@ use bevy::render::render_asset::RenderAssetPlugin;
 use bevy::render::renderer::RenderDevice;
 use bevy::render::{Render, RenderApp, RenderSet};
 use bevy::sprite::Material2dPlugin;
-use vello::{AaSupport, Renderer, RendererOptions};
 
 pub struct VelloRenderPlugin;
 
@@ -65,34 +64,5 @@ impl Plugin for VelloRenderPlugin {
             Update,
             (systems::resize_rendertargets, systems::clear_when_empty),
         );
-    }
-
-    fn finish(&self, app: &mut App) {
-        let render_app = match app.get_sub_app_mut(RenderApp) {
-            Ok(render_app) => render_app,
-            Err(_) => return,
-        };
-
-        let device = render_app
-            .world
-            .get_resource::<RenderDevice>()
-            .expect("bevy_vello: unable to get render device");
-
-        render_app.insert_non_send_resource(BevyVelloRenderer(
-            Renderer::new(
-                device.wgpu_device(),
-                RendererOptions {
-                    surface_format: None,
-                    use_cpu: false,
-                    antialiasing_support: AaSupport {
-                        area: true,
-                        msaa8: false,
-                        msaa16: false,
-                    },
-                    num_init_threads: None,
-                },
-            )
-            .unwrap(),
-        ));
     }
 }
