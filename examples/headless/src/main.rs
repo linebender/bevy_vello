fa1a420546e39ded2b254c911876d06523c57a47
Spencer C. Imbleau <spencer@imbleau.com>
2025-02-09 22:13:36 -0500

    feat: headless example (#112)
    Add a new headless example which takes a screenshot and closes.

---
+++ examples/headless/src/main.rs
@@ -0,0 +1,47 @@
+use bevy::{
+    prelude::*,
+    render::view::screenshot::{save_to_disk, Screenshot, ScreenshotCaptured},
+};
+use bevy_vello::{prelude::*, VelloPlugin};
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            visible: false,
+            skip_taskbar: true,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_plugins(VelloPlugin::default())
+    .add_systems(Startup, (setup, screenshot).chain())
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Camera2d, VelloView));
+
+    // Draw a circle
+    let mut scene = VelloScene::new();
+    scene.fill(
+        peniko::Fill::NonZero,
+        kurbo::Affine::default(),
+        peniko::Color::new([0.0, 1.0, 0.0, 1.0]),
+        None,
+        &kurbo::Circle::new((0.0, 0.0), 50.0),
+    );
+    commands.spawn(VelloSceneBundle { scene, ..default() });
+}
+
+fn screenshot(mut commands: Commands) {
+    let path = "./screenshot.png";
+    commands
+        .spawn(Screenshot::primary_window())
+        .observe(save_to_disk(path))
+        .observe(exit_system);
+}
+
+fn exit_system(_trigger: Trigger<ScreenshotCaptured>, mut exit: EventWriter<AppExit>) {
+    exit.send(AppExit::Success);
+}
