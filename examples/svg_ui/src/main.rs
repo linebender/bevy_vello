fe5ba1a912b715c6eb67e97c2165a19f444803e7
Spencer C. Imbleau <spencer@imbleau.com>
2025-02-20 11:06:02 -0500

    feat: new examples

---
+++ examples/svg_ui/src/main.rs
@@ -0,0 +1,37 @@
+use bevy::{
+    asset::{embedded_asset, AssetMetaCheck},
+    color::palettes::css,
+    prelude::*,
+};
+use bevy_vello::{prelude::*, VelloPlugin};
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(AssetPlugin {
+        meta_check: AssetMetaCheck::Never,
+        ..default()
+    }))
+    .add_plugins(VelloPlugin::default())
+    .add_systems(Startup, load_svg);
+    embedded_asset!(app, "assets/fountain.svg");
+    app.run();
+}
+
+fn load_svg(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+    commands.spawn((Camera2d, VelloView));
+
+    let one_third = Val::Percent(100.0 / 3.0);
+    commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            left: one_third,
+            top: one_third,
+            width: one_third,
+            height: one_third,
+            border: UiRect::all(Val::Px(2.0)),
+            ..default()
+        },
+        BorderColor(css::FUCHSIA.with_alpha(0.5).into()),
+        VelloSvgHandle(asset_server.load("embedded://svg_ui/assets/fountain.svg")),
+    ));
+}
