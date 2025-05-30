fd925a3f0e82b8b7ee5ea889609c25c33c461dac
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-05-30 15:42:45 +0200

    updates view culling example (wip)

--- examples/view_culling/src/main.rs
+++ examples/view_culling/src/main.rs
@@ -18,12 +18,18 @@ fn main() {
     .add_systems(
         Update,
         (
+            left_right,
+            up_down,
+            right_left,
+            down_up,
             simple_animation,
+            draw_aabb_gizmos,
             update_lottie_aab,
-            log_vello_scene_view_visibility,
             update_svg_aab,
             update_text_aab,
-        ),
+            log_visibility,
+        )
+            .chain(),
     );
 
     embedded_asset!(app, "assets/lottie/Tiger.json");
@@ -32,14 +38,41 @@ fn main() {
     app.run();
 }
 
-fn log_vello_scene_view_visibility(
-    query: Query<&ViewVisibility, (Changed<ViewVisibility>, With<VelloScene>)>,
+fn log_visibility(
+    scene_q: Query<&ViewVisibility, (Changed<ViewVisibility>, With<VelloScene>)>,
+    lottie_q: Query<&ViewVisibility, (Changed<ViewVisibility>, With<VelloLottieHandle>)>,
+    svg_q: Query<&ViewVisibility, (Changed<ViewVisibility>, With<VelloSvgHandle>)>,
+    text_q: Query<&ViewVisibility, (Changed<ViewVisibility>, With<VelloTextSection>)>,
 ) {
-    for view_visibility in &query {
+    for view_visibility in &scene_q {
         if view_visibility.get() {
             println!("Scene is visible");
         } else {
             println!("Scene is not visible");
+        }
+    }
+
+    for view_visibility in &lottie_q {
+        if view_visibility.get() {
+            println!("Lottie is visible");
+        } else {
+            println!("Lottie is not visible");
+        }
+    }
+
+    for view_visibility in &svg_q {
+        if view_visibility.get() {
+            println!("SVG is visible");
+        } else {
+            println!("SVG is not visible");
+        }
+    }
+
+    for view_visibility in &text_q {
+        if view_visibility.get() {
+            println!("Text is visible");
+        } else {
+            println!("Text is not visible");
         }
     }
 }
@@ -48,27 +81,34 @@ fn load_view_culling(mut commands: Commands, asset_server: ResMut<AssetServer>) 
     commands.spawn((Camera2d, VelloView));
 
     // TODO: add bb_in_screen_space and bb_in_world_space to VelloScene
-    commands.spawn((
-        VelloScene::new(),
-        Aabb::from_min_max(Vec3::new(-50.0, -50.0, 0.0), Vec3::new(50.0, 50.0, 0.0)),
-    ));
+    commands
+        .spawn(VelloScene::new())
+        .insert(RightLeft)
+        .insert(Aabb::from_min_max(
+            Vec3::new(-50.0, -50.0, 0.0),
+            Vec3::new(50.0, 50.0, 0.0),
+        ));
 
     // You can also use `VelloLottieBundle`
     commands
         .spawn(VelloLottieHandle(
             asset_server.load("embedded://view_culling/assets/lottie/Tiger.json"),
         ))
-        .insert((Transform::from_scale(Vec3::splat(0.5)), Aabb::default()));
+        .insert(Transform::from_scale(Vec3::splat(0.2)))
+        .insert(LeftRight)
+        .insert(Aabb::default());
 
     commands
         .spawn(VelloSvgHandle(
             asset_server.load("embedded://view_culling/assets/svg/fountain.svg"),
         ))
-        .insert((Transform::from_scale(Vec3::splat(5.0)), Aabb::default()));
+        .insert(Transform::from_scale(Vec3::splat(1.0)))
+        .insert(DownUp)
+        .insert(Aabb::default());
 
-    commands.spawn((
-        VelloTextBundle {
-            text: VelloTextSection {
+    commands
+        .spawn((
+            VelloTextSection {
                 value: "View culled text".to_string(),
                 style: VelloTextStyle {
                     font_size: 24.0,
@@ -76,12 +116,10 @@ fn load_view_culling(mut commands: Commands, asset_server: ResMut<AssetServer>) 
                 },
                 ..default()
             },
-            text_anchor: VelloTextAnchor::Center,
-            transform: Transform::default(),
-            ..default()
-        },
-        Aabb::default(),
-    ));
+            VelloTextAnchor::Center,
+        ))
+        .insert(UpDown)
+        .insert(Aabb::default());
 }
 
 fn update_lottie_aab(
@@ -167,6 +205,62 @@ fn simple_animation(mut query_scene: Single<(&mut Transform, &mut VelloScene)>, 
     );
 
     transform.scale = Vec3::lerp(Vec3::ONE * 0.5, Vec3::ONE * 1.0, sin_time);
-    transform.translation = Vec3::lerp(Vec3::X * -1000.0, Vec3::X * 1000.0, sin_time);
     transform.rotation = Quat::from_rotation_z(-std::f32::consts::TAU * sin_time);
+}
+
+#[derive(Component)]
+struct LeftRight;
+
+const ANIMATION_SPEED: f32 = 0.5;
+
+fn left_right(mut query: Query<&mut Transform, With<LeftRight>>, time: Res<Time>) {
+    let sin_time = time.elapsed_secs().sin().mul_add(0.5, 0.5) * ANIMATION_SPEED;
+    for mut transform in query.iter_mut() {
+        transform.translation = Vec3::lerp(Vec3::X * -800.0, Vec3::X * 800.0, sin_time);
+    }
+}
+
+#[derive(Component)]
+struct RightLeft;
+
+fn right_left(mut query: Query<&mut Transform, With<RightLeft>>, time: Res<Time>) {
+    let sin_time = time.elapsed_secs().sin().mul_add(0.5, 0.5) * ANIMATION_SPEED;
+    for mut transform in query.iter_mut() {
+        transform.translation = Vec3::lerp(Vec3::X * 800.0, Vec3::X * -800.0, sin_time);
+    }
+}
+
+#[derive(Component)]
+struct UpDown;
+
+fn up_down(mut query: Query<&mut Transform, With<UpDown>>, time: Res<Time>) {
+    let sin_time = time.elapsed_secs().sin().mul_add(0.5, 0.5) * ANIMATION_SPEED;
+    for mut transform in query.iter_mut() {
+        transform.translation = Vec3::lerp(Vec3::Y * -500.0, Vec3::Y * 500.0, sin_time);
+    }
+}
+
+#[derive(Component)]
+struct DownUp;
+
+fn down_up(mut query: Query<&mut Transform, With<DownUp>>, time: Res<Time>) {
+    let sin_time = time.elapsed_secs().sin().mul_add(0.5, 0.5) * ANIMATION_SPEED;
+    for mut transform in query.iter_mut() {
+        transform.translation = Vec3::lerp(Vec3::Y * 500.0, Vec3::Y * -500.0, sin_time);
+    }
+}
+
+fn draw_aabb_gizmos(aabb_q: Query<(&Aabb, &GlobalTransform)>, mut gizmos: Gizmos) {
+    for (aabb, gtransform) in &aabb_q {
+        let aabb_min = aabb.min();
+        let aabb_max = aabb.max();
+        gizmos.rect_2d(
+            Isometry2d::new(
+                gtransform.translation().xy(),
+                Rot2::radians(gtransform.rotation().to_scaled_axis().z),
+            ),
+            Vec2::new(aabb_max.x - aabb_min.x, aabb_max.y - aabb_min.y) * gtransform.scale().xy(),
+            Color::WHITE,
+        );
+    }
 }
