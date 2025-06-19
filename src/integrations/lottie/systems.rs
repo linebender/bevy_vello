0fae574ce3d8cf40cc54a6c6e30f1a4e64a92d1b
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-06-19 11:58:56 +0200

    lottie run_transitions now works in screen and world space

--- src/integrations/lottie/systems.rs
+++ src/integrations/lottie/systems.rs
@@ -175,7 +175,8 @@ pub fn run_transitions(
         return;
     };
 
-    let pointer_pos = window
+    let pointer_screen_pos = window.cursor_position();
+    let pointer_world_pos = window
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(view, cursor).ok())
         .map(|ray| ray.origin.truncate());
@@ -199,29 +200,45 @@ pub fn run_transitions(
             continue;
         };
 
-        let is_inside = {
-            match pointer_pos {
+        let is_inside = if ui_node.is_some() || screen_space.is_some() {
+            match pointer_screen_pos {
                 Some(pointer_pos) => {
-                    let local_transform = current_asset
+                    let local_center_matrix = current_asset
                         .local_transform_center
                         .compute_matrix()
                         .inverse();
-                    let transform = gtransform.compute_matrix() * local_transform;
+
+                    let transform = gtransform.compute_matrix() * local_center_matrix;
+
                     let mouse_local = transform
                         .inverse()
                         .transform_point3(pointer_pos.extend(0.0));
 
-                    if ui_node.is_some() || screen_space.is_some() {
-                        mouse_local.x <= current_asset.width
-                            && mouse_local.x >= 0.0
-                            && mouse_local.y >= current_asset.height
-                            && mouse_local.y <= 0.0
-                    } else {
-                        mouse_local.x <= current_asset.width
-                            && mouse_local.x >= 0.0
-                            && mouse_local.y >= -current_asset.height
-                            && mouse_local.y <= 0.0
-                    }
+                    mouse_local.x <= current_asset.width
+                        && mouse_local.x >= 0.0
+                        && mouse_local.y <= current_asset.height
+                        && mouse_local.y >= 0.0
+                }
+                None => false,
+            }
+        } else {
+            match pointer_world_pos {
+                Some(pointer_pos) => {
+                    let local_center_matrix = current_asset
+                        .local_transform_center
+                        .compute_matrix()
+                        .inverse();
+
+                    let model_matrix = gtransform.compute_matrix() * local_center_matrix;
+
+                    let mouse_local = model_matrix
+                        .inverse()
+                        .transform_point3(pointer_pos.extend(0.0));
+
+                    mouse_local.x <= current_asset.width
+                        && mouse_local.x >= 0.0
+                        && mouse_local.y <= current_asset.height
+                        && mouse_local.y >= 0.0
                 }
                 None => false,
             }
