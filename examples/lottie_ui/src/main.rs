34587b1e7d7019ccfb939007d44fa6590835be7b
Spencer C. Imbleau <spencer@imbleau.com>
2026-03-09 22:29:56 -0400

    feat: UI sizing tests

--- examples/lottie_ui/src/main.rs
+++ examples/lottie_ui/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     asset::{AssetMetaCheck, embedded_asset},
+    color::palettes::css,
     prelude::*,
 };
 use bevy_vello::{VelloPlugin, prelude::*};
@@ -27,16 +28,34 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn load_lottie(mut commands: Commands, asset_server: ResMut<AssetServer>) {
-    let one_third = Val::Percent(100.0 / 3.0);
-    commands.spawn((
-        Node {
+    let lottie_handle = asset_server.load("embedded://lottie_ui/assets/Tiger.json");
+
+    // Create a 3x3 grid to demonstrate Lottie rendering in different positions
+    commands
+        .spawn(Node {
             position_type: PositionType::Absolute,
-            left: one_third,
-            top: one_third,
-            width: one_third,
-            height: one_third,
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            display: Display::Grid,
+            grid_template_columns: RepeatedGridTrack::percent(3, 33.33),
+            grid_template_rows: RepeatedGridTrack::percent(3, 33.33),
             ..default()
-        },
-        UiVelloLottie(asset_server.load("embedded://lottie_ui/assets/Tiger.json")),
-    ));
+        })
+        .with_children(|parent| {
+            // Create 9 cells with Lottie animations
+            for _ in 0..9 {
+                parent.spawn((
+                    Node {
+                        border: UiRect::all(Val::Px(2.0)),
+                        padding: UiRect::all(Val::Px(10.0)),
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        ..default()
+                    },
+                    BorderColor::all(css::FUCHSIA.with_alpha(0.5)),
+                    BackgroundColor(css::DARK_SLATE_GRAY.with_alpha(0.3).into()),
+                    UiVelloLottie(lottie_handle.clone()),
+                ));
+            }
+        });
 }
